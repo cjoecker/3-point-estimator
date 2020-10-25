27535cca3a079c5bd8ed802c37f841dffc036d52
Christian Jöcker <kontakt@cjoecker.de>
2020-10-25 19:38:48 +0100

    add probability calculation

--- src/tasksTable/components/ResultsBar.tsx
+++ src/tasksTable/components/ResultsBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import {FormControl, Grid, MenuItem, Select, Typography} from '@material-ui/core';
 import {useTaskEstimationContext} from '../../hooks/useTasksEstimations';
@@ -24,6 +24,8 @@ const ResultsBar = () => {
 
 	const {tasks} = useTaskEstimationContext();
 
+	const [probability, setProbability] = useState(3);
+
 	return (
 		<div className={classes.mainDiv}>
 			<Grid container direction='row' justify='center' alignItems='center' spacing={2}>
@@ -33,7 +35,7 @@ const ResultsBar = () => {
 							Probability
 						</Typography>
 						<FormControl variant='outlined'>
-							<Select value={3}>
+							<Select value={probability} onChange={e => setProbability(e.target.value as number)}>
 								<MenuItem value={4}>99,99 %</MenuItem>
 								<MenuItem value={3}>99,73 %</MenuItem>
 								<MenuItem value={2}>95,45 %</MenuItem>
@@ -49,7 +51,7 @@ const ResultsBar = () => {
 							<b>
 								<u>Total Time</u>
 							</b>
-							{getTimeEstimation(tasks, 2)}
+							{getTimeEstimation(tasks, probability)}
 						</Grid>
 					</Typography>
 				</Grid>
