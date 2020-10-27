c46f1666289d3d42477a3d086acc662274e6b77d
Christian Jöcker <kontakt@cjoecker.de>
2020-10-27 22:09:52 +0100

    Updates

---
+++ precache-manifest.4e96f7dffcbdbff53f5905476a7a909a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82946fe0125dc0412ee3adb8a42b2e5f",
+    "url": "/3-point-estimator/index.html"
+  },
+  {
+    "revision": "adb808a710698578c40f",
+    "url": "/3-point-estimator/static/css/main.7b1750f5.chunk.css"
+  },
+  {
+    "revision": "70bc0dfa7da5c95ac71e",
+    "url": "/3-point-estimator/static/js/2.0e3d63cf.chunk.js"
+  },
+  {
+    "revision": "b425ae70647b4fb7e3b524da526f0821",
+    "url": "/3-point-estimator/static/js/2.0e3d63cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb808a710698578c40f",
+    "url": "/3-point-estimator/static/js/main.bbfd6046.chunk.js"
+  },
+  {
+    "revision": "d64baf2ca6d1f09efb10",
+    "url": "/3-point-estimator/static/js/runtime-main.cbb4b707.js"
+  }
+]);
